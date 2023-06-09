7fa1cc14b9dc3cd1b208cda5dbce19474a7a45ba
LanaVol <svolosh4yk@gmail.com>
2023-06-09 19:56:55 +0300

    Home page&component cardItem

--- src/components/Home/Home.page.jsx
+++ src/components/Home/Home.page.jsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 import { Box, Container, Grid, ImageListItem } from "@mui/material";
 import { GridWrapper } from "../GridWrapper";
 import { ItemBanner } from "../ItemBanner";
+import { ItemSmallCard } from "../ItemSmallCard";
+import banner from "../../image/banner.jpg";
+import letters from "../../image/letters.jpg";
 
-export const HomePage = () => {
+export const HomePage = ({ data }) => {
   return (
     <>
       <Box sx={{ height: "400px", overflow: "hidden", position: "relative" }}>
-        <Image alt="Banner Image" fill={true} style={{ objectFit: "cover" }} />
+        <Image
+          src={banner}
+          alt="Banner Image"
+          fill={true}
+          style={{ objectFit: "cover" }}
+        />
       </Box>
 
       <Container>
@@ -22,6 +30,7 @@ export const HomePage = () => {
           }}
         >
           <Image
+            src={letters}
             style={{ objectFit: "contain", display: "block" }}
             alt="Events letters"
           />
@@ -31,6 +40,58 @@ export const HomePage = () => {
           title="Explore the World of Events"
           subtitle="Discover the most fascinating events happening worldwide with us!"
         />
+
+        <Grid
+          container
+          rowSpacing={1}
+          sx={{ padding: "20px 0", marginBottom: "100px" }}
+        >
+          {data.cities
+            .filter((city) => city.showOnHomePage)
+            .map((ev, index) => (
+              <GridWrapper item key={ev.id} isEven={index % 2 === 0}>
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    maxHeight: "500px",
+                    overflow: "hidden",
+                    backgroundColor: "red",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                  alignItems={`${index % 2 === 0 ? "end" : "start"}`}
+                >
+                  <Link href={`/events/${ev.city.toLowerCase()}`}>
+                    <ImageListItem
+                      sx={{
+                        width: "80%",
+                        height: "auto",
+                        transition: "transform 500ms linear",
+                        "&:hover": { transform: "scale(1.1)" },
+                      }}
+                    >
+                      <Image
+                        src={ev.imagePath}
+                        alt={ev.title}
+                        width={800}
+                        height={500}
+                        priority={true}
+                        style={{ width: "auto", height: "auto" }}
+                      />
+                    </ImageListItem>
+                  </Link>
+                </Box>
+                <ItemSmallCard
+                  title={ev.title}
+                  description={ev.description}
+                  country={ev.country}
+                  totalEvents={ev.totalEvents}
+                  population={ev.population}
+                />
+              </GridWrapper>
+            ))}
+        </Grid>
       </Container>
     </>
   );
