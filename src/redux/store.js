7bfd6ac63af3f4a995c6f7a83f873106315db512
LanaVol <svolosh4yk@gmail.com>
2023-06-12 23:23:31 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import {
   persistStore,
   FLUSH,
@@ -10,6 +10,9 @@ import {
 } from "redux-persist";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import authSlice from "./auth/authSlice";
+import eventSlice from "./cities/event.slice";
+import themeSlice from "./theme/themeSlice";
 
 const persistAuthConfig = {
   key: "auth",
@@ -21,3 +24,19 @@ const persistThemeConfig = {
   key: "mode",
   storage,
 };
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistAuthConfig, authSlice),
+    theme: persistReducer(persistThemeConfig, themeSlice),
+    events: eventSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
