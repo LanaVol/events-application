85bbba4cdd81ba99e51d8f6d9dcf90479a5f9a57
LanaVol <svolosh4yk@gmail.com>
2023-06-13 16:16:51 +0300

    update pages

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../redux/store.js";
+import { injectStore } from "@/config/axios.api.js";
+import { Layout } from "@/components/Layout/Layout";
+injectStore(store);
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
+    </Provider>
+  );
+}
