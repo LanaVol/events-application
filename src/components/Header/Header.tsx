ab7f72b5bc1662d4a9a7b49da8c3f387b402a3a8
LanaVol <svolosh4yk@gmail.com>
2023-07-07 20:26:31 +0300

    create mobileMenu & update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,17 @@
-import { useSelector, useDispatch } from "react-redux";
+import { MobileMenu } from "./MobileMenu";
 import {
   Box,
-  Button,
   Container,
-  IconButton,
   Typography,
   useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import { AccountCircle, Logout } from "@mui/icons-material";
-import { AuthOperations } from "../../redux/auth/auth.operations";
+import { MenuNavigationLink } from "../MenuNavigationLink";
 import { ThemeToggle } from "../index";
-import { AppDispatch, RootState } from "../../redux/store";
-import { CustomButton } from "../index";
 
 export const Header = (): JSX.Element => {
-  const isLogged = useSelector((state: RootState) => state.auth.isLogged);
-  const dispatch: AppDispatch = useDispatch();
+  const isNonMobileScreens = useMediaQuery("(max-width:600px)");
   const theme = useTheme();
 
   return (
@@ -25,80 +21,39 @@ export const Header = (): JSX.Element => {
           background: theme.palette.background.gradientHeaderBg,
           color: theme.palette.text.primary,
           boxShadow: "0px 5px 2.9px rgba(12, 12, 12, 0.197)",
-          // position: "fixed",
-          // zIndex: 20,
-          // width: "100%",
         }}
       >
-        <Container maxWidth="xl">
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Typography
-              variant="h2"
-              fontSize="50px"
-              fontWeight="700"
-              sx={{
-                textTransform: "uppercase",
-                padding: "10px 0px",
-                letterSpacing: "0.5rem",
-              }}
-            >
-              Events
-            </Typography>
-
+        {isNonMobileScreens ? (
+          <Container maxWidth="xl">
+            <MobileMenu />
+          </Container>
+        ) : (
+          <Container maxWidth="xl">
             <Box
               sx={{
-                width: "30%",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "space-between",
-                gap: "2rem",
               }}
             >
+              <Typography
+                variant="h2"
+                fontSize="42px"
+                fontWeight="700"
+                sx={{
+                  textTransform: "uppercase",
+                  padding: "20px 0px",
+                }}
+              >
+                Events
+              </Typography>
+
+              <MenuNavigationLink />
+
               <ThemeToggle />
-
-              {isLogged ? (
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    gap: "2rem",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                      gap: "0.5rem",
-                    }}
-                  >
-                    <AccountCircle fontSize="large" />
-                    <Typography variant="subtitle1">Hello, Admin!</Typography>
-                  </Box>
-
-                  <IconButton onClick={() => dispatch(AuthOperations.logout())}>
-                    <Logout />
-                  </IconButton>
-
-                  <CustomButton
-                    text="Logout"
-                    onClick={() => {
-                      dispatch(AuthOperations.logout());
-                    }}
-                    startIcon={<Logout />}
-                  />
-                </Box>
-              ) : null}
             </Box>
-          </Box>
-        </Container>
+          </Container>
+        )}
       </Box>
     </header>
   );
