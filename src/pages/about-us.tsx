d6a0aa405d0f286f7b41d88efc8741f78a8bd1cf
LanaVol <svolosh4yk@gmail.com>
2023-07-18 23:10:05 +0300

    change styles aboutUs page

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,6 +1,14 @@
 import { Header } from "../components/Header/Header";
 import { MenuNavigation } from "../components/MenuNavigation";
-import { Box, Container, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { MainTitle } from "../components";
+import blob2 from "../image/blob2.svg";
 
 const list = [
   { title: "Home", path: "/", iconName: "home" },
@@ -9,28 +17,58 @@ const list = [
 
 const AboutUsPage = (): JSX.Element => {
   const theme = useTheme();
+  const smallerScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <>
       <Header />
-      <Container maxWidth="xl" sx={{ flex: 1 }}>
-        <Box sx={{ color: theme.palette.text.primary }}>
-          <Box sx={{ padding: "1rem 0" }}>
+      <Box
+        sx={{
+          flex: 1,
+          backgroundImage: `url(${blob2.src})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <Container
+          maxWidth="xl"
+          sx={{
+            flex: 1,
+          }}
+        >
+          <Box
+            sx={{
+              color: theme.palette.text.primary,
+            }}
+          >
             <MenuNavigation list={list} />
-          </Box>
 
-          <Typography variant="h4">About us Page</Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Typography>
-        </Box>
-      </Container>
+            <MainTitle title="About Us Page" showArrow={false} />
+
+            <Typography
+              variant="subtitle1"
+              sx={{
+                textAlign: "justify",
+                width: smallerScreen ? "100%" : "50%",
+                margin: "0 auto",
+                lineHeight: "2rem",
+                fontSize: "20px",
+                padding: "3rem 1rem",
+              }}
+            >
+              Welcome to our website, where you can find information about
+              various exciting events happening around the world. Here, you can
+              explore a diverse range of events, from concerts and exhibitions
+              to sports competitions and cultural festivals. On our website, you
+              will discover an up-to-date event calendar featuring detailed
+              descriptions? dates, venues, and other valuable information.
+              Browse through our event listings, choose what interests you, and
+              join in on the thrilling experiences that will leave a lasting
+              impression.
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 };
